6495114e4939618f0b7193fef2084375304f0a70
Clay <claycarney7@gmail.com>
2022-06-13 18:06:22 -0400

    practice on let vs. const usage; declaring multiple variables;

---
+++ javascript.js
@@ -0,0 +1,34 @@
+ // This is some javascript code
+//  console.log("Hello world!")
+
+//  let name = "Mosh";  //using "name" as a variable name is now deprecated, meaning it'll still work but its use is discouraged
+//  console.log(name);
+
+ // Cannot be a reserved keyword
+ // Should be meaningful
+ // Cannot start with a number(1name)
+ // Cannot contain a space or hyphen (-)
+ // Are case-sensitive
+
+ // How to declare multiple variables( best practice)
+ //let firstName, lastName, middleName; //not this
+
+//  let firstName = "Clay"; 
+//  let lastName = "Baruch";  //like these
+//  let middleName = "Carney"; //make sure to end line in semi-colon
+
+ 
+// Best Practice let vs. const: Use const by default when you don't need to reassign a variable, otherwise use let. 
+//  let interestRate = 0.3;
+//  interestRate = 1; // This is reassigning the value of interestRate from 0.3 to 1, and will appear so in the console
+//  console.log(interestRate);
+
+//  const percentageRate = 0.4;
+//  percentageRate = 2.5;  // You cannot reassign a const and so this will throw an error in the console
+//  console.log(percentageRate);
+
+
+
+               
+
+                
