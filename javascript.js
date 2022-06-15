43384f5c4a0fab9d56d9fbb18827a6d4e90341a9
Clay <claycarney7@gmail.com>
2022-06-14 23:52:23 -0400

    comment out previous lessons and separate sections to make it more readable

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@
 
 //  let name = "Mosh";  //using "name" as a variable name is now deprecated, meaning it'll still work but its use is discouraged
 //  console.log(name);
-
+//--------------------------------------------------------------------------------------------------------------------------------
        // Cannot be a reserved keyword
       // Should be meaningful
      // Cannot start with a number(1name)
     // Cannot contain a space or hyphen (-)
    // Are case-sensitive
-
+//--------------------------------------------------------------------------------------------------------------------------------
   // How to declare multiple variables( best practice)
  //let firstName, lastName, middleName; //not this
 
@@ -19,14 +19,21 @@
 
  
 // Best Practice let vs. const: Use const by default when you don't need to reassign a variable, otherwise use let. 
+
+
+// Using let------------------------------------------------------------------------------------------------------------------
+
 //  let interestRate = 0.3;
 //  interestRate = 1; // This is reassigning the value of interestRate from 0.3 to 1, and will appear so in the console
 //  console.log(interestRate);
 
+
+// Using const-----------------------------------------------------------------------------------------------------------------
+
 //  const percentageRate = 0.4;
 //  percentageRate = 2.5;  // You cannot reassign a const and so this will throw an error in the console
 //  console.log(percentageRate);
-
+//------------------------------------------------------------------------------------------------------------------------------
 
 
                     // Primitive Types
@@ -36,45 +43,58 @@
                 // undefined
                // null
 
-let name = "Mosh"; // String Literal
-let age = 30; // Number Literal
-let isApproved = false; // Boolean Literal
-let firstName; // let firstName = undefined;
-let lastName = null; // Can be used when you want to clear the value of a variable
+// let name = "Mosh"; // String Literal
+// let age = 30; // Number Literal
+// let isApproved = false; // Boolean Literal
+// let firstName; // let firstName = undefined;
+// let lastName = null; // Can be used when you want to clear the value of a variable
 
-           // Reference Types
-          // Object
-         // Array
-        // Function
+//--------------------------------------------------------------------------------------------------
 
-let person = {      // Object Literal
-    name: "Mosh",  //key-value pair or property-string pair
-    age: 30       //key-value pair or property-string pair
-}; 
+//            // Reference Types
+//           // Object
+//          // Array
+//         // Function
 
 
-// Dot Notation
-person.name = "John";
+// Objects------------------------------------------------------------------------------------------
 
-// Bracket Notation
-let slection = "name";
-person[slection] = "Mary";
+// let person = {      // Object Literal
+//     name: "Mosh",  //key-value pair or property-string pair
+//     age: 30       //key-value pair or property-string pair
+// }; 
 
-   console.log(person.name);  
+
+// // Dot Notation
+// person.name = "John";
+
+// // Bracket Notation
+// let slection = "name";
+// person[slection] = "Mary";
+
+//    console.log(person.name);  
    
 
+// Arrays-------------------------------------------------------------------------------------------
 
-let selectedColors = ["red","blue"];    // Array Literal
-selectedColors[2] = "green";           // How to add more items to a previously declared array
-console.log(selectedColors);          // An array is a data structure you can use to represent a list of items
-console.log(selectedColors[0]);      // You can specify which item in the index by using [] filled with the number in the index, starting at 0
+// let selectedColors = ["red","blue"];    // Array Literal
+// selectedColors[2] = "green";           // How to add more items to a previously declared array
+// console.log(selectedColors);          // An array is a data structure you can use to represent a list of items
+// console.log(selectedColors[0]);      // You can specify which item in the index by using [] filled with the number in the index, starting at 0
 
-selectedColors[3] = "brown";       // If you add extra items after calling for an array list using console.log or something else, it will not appear in the called list unless followed by another console.log
-console.log(selectedColors);
+// selectedColors[3] = "brown";       // If you add extra items after calling for an array list using console.log or something else, it will not appear in the called list unless followed by another console.log
+// console.log(selectedColors);
 
-selectedColors[4] = 889;        // You can have different types in an array, i.e. this number
-console.log(selectedColors);
-                              // The objects in an array as well as the size of the array are dynamic, meaning they can be changed, added to, etc.
-                             // An array is technically an object in javascript, so dot and bracket notation can also be used on arrays
-console.log(selectedColors.length);
-                           // The length property returns the number of items or elements in an array
+// selectedColors[4] = 889;        // You can have different types in an array, i.e. this number
+// console.log(selectedColors);
+//                               // The objects in an array as well as the size of the array are dynamic, meaning they can be changed, added to, etc.
+//                              // An array is technically an object in javascript, so dot and bracket notation can also be used on arrays
+// console.log(selectedColors.length);
+                              // The length property returns the number of items or elements in an array
+
+
+
+// Functions-------------------------------------------------------------------------------------------------------------
+function greet() {
+    
+}
