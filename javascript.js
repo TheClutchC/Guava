9ae1f170d227a53fb2187973e49fcb6d1cc7cf3c
TheClutchC <claycarney7@gmail.com>
2022-07-12 20:14:46 -0400

    exercise using odd and even numbers

--- javascript.js
+++ javascript.js
@@ -605,3 +605,22 @@
 //   else 
 //     console.log("Points", points);
 // }
+
+
+// Even and Odd Numbers----------------------------------------------------------------
+
+//
+//
+//
+
+showNumbers(10);
+
+function showNumbers(limit) {
+     for (let i = 0; i <= limit; i++) {
+
+          if(i % 2 === 0)
+               console.log(i, "EVEN");
+          else
+               console.log(i, "ODD");
+     }
+}
